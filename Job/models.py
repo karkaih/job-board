bb8ad663a773fcf62b3b109c00f03b3f17ccf6fc
karkaih <achrafkarkaih@gmail.com>
2020-11-17 19:41:37 +0100

    DB

--- Job/models.py
+++ Job/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+'''
+django  model field :
+    -html widget
+    -validation
+    -db size
+'''
+Job_Type = (
+('Full Time','Full Time'), 
+('Part Time','Part Time'), 
+
+)
+class Job(models.Model):#Table
+    title = models.CharField(max_length=100)#colmun
+    #location
+    job_type = models.CharField(max_length=15,choices=Job_Type)    
+    
+    description = models.TextField(max_length=1000)
+    publish = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=1)
+
+    def __str__(self) :
+        return self.title
