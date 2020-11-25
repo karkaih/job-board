46dc6ad597f0e088d3339a398d2809d801b1cb46
karkaih <achrafkarkaih@gmail.com>
2020-11-25 17:40:32 +0100

    frontend

--- Job/urls.py
+++ Job/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include,path
 
 from . import views
-
+app_name="Job"
 urlpatterns = [
     path('',views.job_list),
-    path('<int:id>',views.job_detail),
+    path('<int:id>',views.job_detail,name='jobdetail'),
 ]
--- project/urls.py
+++ project/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('jobs/', include('Job.urls')),
+    path('jobs/', include('Job.urls',namespace='jobs')),
 
 ]
 
