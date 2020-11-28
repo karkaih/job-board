3da492f9cad959fa67d1423f121c4a0bb483b849
karkaih <achrafkarkaih@gmail.com>
2020-11-28 21:24:31 +0100

    add_job

--- Job/urls.py
+++ Job/urls.py
@@ -3,6 +3,8 @@ from django.urls import include,path
 from . import views
 app_name="Job"
 urlpatterns = [
-    path('',views.job_list),
+    path('',views.job_list,name='job_list'),
+    path('add',views.add_job,name='add_job'),
     path('<str:slug>',views.job_detail,name='jobdetail'),
+     
 ]
--- Job/views.py
+++ Job/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.core.paginator import Paginator
 from .models import Job
-from .form import ApplyForm
+from .form import ApplyForm ,JobForm
+from django.urls import reverse
 # Create your views here.
 
 def job_list(request) :
     job_list= Job.objects.all()
 
-    paginator = Paginator(job_list, 1) 
+    paginator = Paginator(job_list, 3) 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -34,3 +35,22 @@ def job_detail(request,slug) :
     context = {'jobs' : job_detail , 'form':form}
     print(job_list)
     return render(request,'Job/job_detail.html',context)
+
+
+
+def add_job(request) :
+
+    if request.method == 'POST':
+        form =JobForm(request.POST,request.FILES)
+        if form.is_valid():
+            myform = form.save(commit=False)
+            myform.owner=request.user
+            myform.save()
+            return redirect(reverse('jobs:job_list'))
+
+    else :
+        form = JobForm()
+
+
+    
+    return render(request,'Job/add_job.html',{'form':form})
