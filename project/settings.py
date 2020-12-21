436615b92b5fc1dce5e9709462fe7d2f89279ac8
karkaih <achrafkarkaih@gmail.com>
2020-12-21 20:32:55 +0100

    mb

--- project/settings.py
+++ project/settings.py
@@ -136,9 +136,4 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / "media"
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER ='achraf04boys@gmail.com'
-EMAIL_HOST_PASSWORD='kbukcgvkerbfkden'
-EMAIL_USE_TLS=True
-EMAIL_PORT='587'
+
