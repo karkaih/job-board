9d290d68797c63b81a6e282b30efe922d5b8f8d5
karkaih <achrafkarkaih@gmail.com>
2020-12-27 18:32:35 +0100

    logout

--- Account/urls.py
+++ Account/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name="Account"
 urlpatterns = [
     path('signup',views.signup,name='signup'),
-    path('profile',views.profile,name='profile'),
+    path('profile/',views.profile,name='profile'),
     path('profile/edit',views.profile_edit,name='profile_edit'),
     
      
